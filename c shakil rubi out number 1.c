ae232d4881127ddf601b083192ce5be791d21031
Shakil <81988841+shakil4050@users.noreply.github.com>
2022-11-11 10:22:38 +0600

    Add files via upload

---
+++ c shakil rubi out number 1.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+
+
+char xyz[8];
+
+
+printf("Enter your first name: \n");
+
+
+scanf("%s", xyz);
+
+
+if(!strcmp(xyz,"shakil"))
+{
+    printf("017");
+}
+if(!strcmp(xyz,"rubi"))
+{
+    printf("109");
+}
+  return 0;
+}
